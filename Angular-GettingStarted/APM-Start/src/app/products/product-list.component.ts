0e0a70128c12b9b35cccbb84a8611097cca1ec4c
ChrisAndersen <Christian.Andersen@dhsoha.state.or.us>
2023-06-12 15:17:47 -0700

    Add Service injection for ProductService Remove hard coded array, instead call ProductService

--- Angular-GettingStarted/APM-Start/src/app/products/product-list.component.ts
+++ Angular-GettingStarted/APM-Start/src/app/products/product-list.component.ts
@@ -5,7 +5,8 @@ import { ProductService } from "./product.service";
 @Component({
     selector: "pm-products",
     templateUrl: "./product-list.component.html",
-    styleUrls: ["./product-list.component.css"]
+    styleUrls: ["./product-list.component.css"],
+    providers: [ProductService]
 })
 export class ProductListComponent implements OnInit {
 
@@ -19,7 +20,7 @@ export class ProductListComponent implements OnInit {
     */
 
     //Shorthand constructor service injectable example:
-    constructor(private productService: ProductService){
+    constructor(private productService: ProductService) {
     }
 
     pageTitle: string = "Product List";
@@ -27,6 +28,8 @@ export class ProductListComponent implements OnInit {
     imageMargin: number = 2;
     showImage: boolean = false;
     private _listFilter: string = "";
+    products: IProduct[] = [];
+
     get listFilter(): string {
         return this._listFilter;
     }
@@ -35,59 +38,6 @@ export class ProductListComponent implements OnInit {
         console.log("Setter change to:" + value);
 
     }
-    products: IProduct[] =
-        [
-            {
-                "productId": 1,
-                "productName": "Leaf Rake",
-                "productCode": "GDN-0011",
-                "releaseDate": "March 19, 2021",
-                "description": "Leaf rake with 48-inch wooden handle.",
-                "price": 19.95,
-                "starRating": 3.2,
-                "imageUrl": "assets/images/leaf_rake.png"
-            },
-            {
-                "productId": 2,
-                "productName": "Garden Cart",
-                "productCode": "GDN-0023",
-                "releaseDate": "March 18, 2021",
-                "description": "15 gallon capacity rolling garden cart",
-                "price": 32.99,
-                "starRating": 4.2,
-                "imageUrl": "assets/images/garden_cart.png"
-            },
-            {
-                "productId": 5,
-                "productName": "Hammer",
-                "productCode": "TBX-0048",
-                "releaseDate": "May 21, 2021",
-                "description": "Curved claw steel hammer",
-                "price": 8.9,
-                "starRating": 4.8,
-                "imageUrl": "assets/images/hammer.png"
-            },
-            {
-                "productId": 8,
-                "productName": "Saw",
-                "productCode": "TBX-0022",
-                "releaseDate": "May 15, 2021",
-                "description": "15-inch steel blade hand saw",
-                "price": 11.55,
-                "starRating": 3.7,
-                "imageUrl": "assets/images/saw.png"
-            },
-            {
-                "productId": 10,
-                "productName": "Video Game Controller",
-                "productCode": "GMG-0042",
-                "releaseDate": "October 15, 2020",
-                "description": "Standard two-button video game controller",
-                "price": 35.95,
-                "starRating": 4.6,
-                "imageUrl": "assets/images/xbox-controller.png"
-            }
-        ];
 
     performFilter(): IProduct[] {
         /*
@@ -110,6 +60,7 @@ export class ProductListComponent implements OnInit {
 
     ngOnInit(): void {
         console.log('Triggered OnInit');
+        this.products = this.productService.getProducts();
     }
 
     /*
