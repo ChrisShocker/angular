42e807217af9a7dd0bd7102b91286ab988a8d7ab
ChrisAndersen <Christian.Andersen@dhsoha.state.or.us>
2023-06-14 13:39:31 -0700

    catch and handle parameter from router

--- Angular-GettingStarted/APM-Start/src/app/products/products-detail/products-detail.component.ts
+++ Angular-GettingStarted/APM-Start/src/app/products/products-detail/products-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'pm-products-detail',
@@ -9,9 +10,23 @@ export class ProductsDetailComponent implements OnInit {
 
   pageTitle: string = "Product Details"
 
-  constructor() { }
+  //set activated route as dependency
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    //from RouterModule, defined route taking in 'id' param: {path: 'products/:id', component: ProductsDetailComponent}
+    //from product-list.comp: passing in 'id':
+    /*
+      <a [routerLink]="['/products', product.productId]"> 
+        {{product.productName}} 
+      </a>
+    */
+
+    //Get the route parameter passed in from product-list comp
+    //using the snapshot approach since paramter won't change while comp is displayed
+    //since id is a string, we cast to a number: Number()
+    const id = Number(this.route.snapshot.paramMap.get("id"));
+    this.pageTitle += `: ${id}`
   }
 
 }
