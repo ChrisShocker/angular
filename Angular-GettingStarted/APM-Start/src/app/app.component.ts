c52e8733cf97cf6853888c1d60a7b6672cc6aa9d
ChrisAndersen <Christian.Andersen@dhsoha.state.or.us>
2023-06-14 13:39:12 -0700

    Remove commented code

--- Angular-GettingStarted/APM-Start/src/app/app.component.ts
+++ Angular-GettingStarted/APM-Start/src/app/app.component.ts
@@ -3,13 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'pm-root',
   templateUrl: './app.component.html',
-  // template: `
-  // <div>
-  // <h1>
-  //   {{pageTitle}}
-  // </h1>
-  // </div> 
-  // `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
