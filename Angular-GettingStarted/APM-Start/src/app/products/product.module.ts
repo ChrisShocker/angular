ca29fc67f8cb07a86d523cccc78ece11bb87c10d
ChrisAndersen <Christian.Andersen@dhsoha.state.or.us>
2023-06-15 11:25:45 -0700

    add declarations/imports to feature module

--- Angular-GettingStarted/APM-Start/src/app/products/product.module.ts
+++ Angular-GettingStarted/APM-Start/src/app/products/product.module.ts
@@ -1,12 +1,35 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { ProductListComponent } from './product-list.component';
+import { ProductsDetailComponent } from './products-detail/products-detail.component';
+import { ConvertToSpacesPipe } from '../shared/convert-to-spaces.pipe';
+import { starComponent } from '../shared/star.component';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { ProductDetailGuard } from './products-detail/product-detail.guard';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProductListComponent,
+    ProductsDetailComponent,
+    ConvertToSpacesPipe,
+    starComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    /*Since Router has already been imported as a service in AppModule
+      we use "forChild" and include the paths specific to this feature module
+      forChild doesn't reregister the router service 
+      and only declares directives and configures routes
+    */
+    RouterModule.forChild([
+      {path: 'products', component: ProductListComponent},
+      {path: 'products/:id',
+       canActivate: [ProductDetailGuard],
+       component: ProductsDetailComponent},
+
+    ])
   ]
 })
 export class ProductModule { }
