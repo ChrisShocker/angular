25e19c5a01c798501937c4ef934d53b9733759a2
ChrisAndersen <Christian.Andersen@dhsoha.state.or.us>
2023-06-15 11:58:02 -0700

    segment modules

--- Angular-GettingStarted/APM-Start/src/app/products/product.module.ts
+++ Angular-GettingStarted/APM-Start/src/app/products/product.module.ts
@@ -1,10 +1,6 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { ProductListComponent } from './product-list.component';
 import { ProductsDetailComponent } from './products-detail/products-detail.component';
-import { ConvertToSpacesPipe } from '../shared/convert-to-spaces.pipe';
-import { starComponent } from '../shared/star.component';
-import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { ProductDetailGuard } from './products-detail/product-detail.guard';
 import { SharedModule } from '../shared/shared.module';
@@ -13,12 +9,8 @@ import { SharedModule } from '../shared/shared.module';
   declarations: [
     ProductListComponent,
     ProductsDetailComponent,
-    ConvertToSpacesPipe,
-    starComponent
   ],
   imports: [
-    CommonModule,
-    FormsModule,
     /*Since Router has already been imported as a service in AppModule
       we use "forChild" and include the paths specific to this feature module
       forChild doesn't reregister the router service 
