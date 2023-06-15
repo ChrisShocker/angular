92fdedefe4b5a80a65ea53b964f86d46301c1c81
ChrisAndersen <Christian.Andersen@dhsoha.state.or.us>
2023-06-15 11:32:03 -0700

    remove unused imports

--- Angular-GettingStarted/APM-Start/src/app/app.module.ts
+++ Angular-GettingStarted/APM-Start/src/app/app.module.ts
@@ -3,13 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ProductListComponent } from './products/product-list.component';
-import { FormsModule } from '@angular/forms';
-import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
-import { starComponent } from './shared/star.component';
-import { ProductsDetailComponent } from './products/products-detail/products-detail.component';
 import { WelcomeComponent } from './home/welcome.component';
 import { RouterModule } from '@angular/router';
-import { ProductDetailGuard } from './products/products-detail/product-detail.guard';
 import { ProductModule } from './products/product.module';
 
 @NgModule({
