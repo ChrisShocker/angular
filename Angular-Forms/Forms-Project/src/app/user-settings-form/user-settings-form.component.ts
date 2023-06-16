61a0f75145f2ab631bc2e93fda7def5ca7c6f42e
ChrisAndersen <Christian.Andersen@dhsoha.state.or.us>
2023-06-16 15:40:48 -0700

    create and init userSettings Object

--- Angular-Forms/Forms-Project/src/app/user-settings-form/user-settings-form.component.ts
+++ Angular-Forms/Forms-Project/src/app/user-settings-form/user-settings-form.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {UserSettings} from '../data/user-settings';
 
 @Component({
   selector: 'app-user-settings-form',
   templateUrl: './user-settings-form.component.html',
   styleUrls: ['./user-settings-form.component.css']
 })
-export class UserSettingsFormComponent {
+export class UserSettingsFormComponent implements OnInit{
+  userSettings: UserSettings = {
+    name: 'Milton',
+    emailOffers: false,
+    interfaceStyle: 'dark',
+    subscriptionType: 'Annual',
+    notes: 'notes'
+  }
+
+  ngOnInit(): void {
+      
+  }
 
 }
