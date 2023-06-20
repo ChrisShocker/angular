c44c68c00e0533e501c804194fd07483af58bb70
ChrisAndersen <Christian.Andersen@dhsoha.state.or.us>
2023-06-20 11:13:03 -0700

    add http access to data service

--- Angular-Forms/Forms-Project/src/app/data/data.service.ts
+++ Angular-Forms/Forms-Project/src/app/data/data.service.ts
@@ -1,16 +1,20 @@
 import { Injectable } from '@angular/core';
 import { UserSettings } from './user-settings';
 import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   //handle user object sent from form 
-  postUserSettingsForm(UserSettings: UserSettings): Observable<UserSettings>{
-    return of(UserSettings);
+  //change Observable type to any, to handle whatever is posted from form
+  postUserSettingsForm(UserSettings: UserSettings): Observable<any>{
+    //http returns an observable, no type casting needed
+    return this.http.post('url' , UserSettings);
+    // return of(UserSettings);
   }
 }
