40c883fd42e161b3ac48a06e1f7b87e98af107ff
ChrisAndersen <Christian.Andersen@dhsoha.state.or.us>
2023-06-20 11:21:06 -0700

    add web service for returning http responses

--- Angular-Forms/Forms-Project/src/app/data/data.service.ts
+++ Angular-Forms/Forms-Project/src/app/data/data.service.ts
@@ -8,13 +8,16 @@ import { HttpClient } from '@angular/common/http';
 })
 export class DataService {
 
+  //url from the putsReq website that allows us to fake http responses 
+  private putsReq: string = 'https://putsreq.com/H1az6Z0D7SnODXVzZGBW';
+
   constructor(private http: HttpClient) { }
 
   //handle user object sent from form 
   //change Observable type to any, to handle whatever is posted from form
   postUserSettingsForm(UserSettings: UserSettings): Observable<any>{
     //http returns an observable, no type casting needed
-    return this.http.post('url' , UserSettings);
+    return this.http.post(this.putsReq , UserSettings);
     // return of(UserSettings);
   }
 }
