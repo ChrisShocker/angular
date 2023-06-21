a9327bbf66ff42abbf9dd2368f266288af8ea197
Christian Andersen <Christian.Andersen@dhsoha.state.or.us>
2023-06-21 16:11:25 -0700

    add user component

--- Angular-CLI/angular-routing/src/app/admin/admin.module.ts
+++ Angular-CLI/angular-routing/src/app/admin/admin.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
 import { EmailBlastComponent } from './email-blast/email-blast.component';
+import { UserComponent } from './user/user.component';
 
 
 @NgModule({
   declarations: [
     AdminComponent,
-    EmailBlastComponent
+    EmailBlastComponent,
+    UserComponent
   ],
   imports: [
     CommonModule,
