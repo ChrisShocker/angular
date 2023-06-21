c2ba74fa35e18c27e1f27b2244ec9279e2f2c121
Christian Andersen <Christian.Andersen@dhsoha.state.or.us>
2023-06-21 16:23:32 -0700

    add children routing paths

--- Angular-CLI/angular-routing/src/app/admin/admin-routing.module.ts
+++ Angular-CLI/angular-routing/src/app/admin/admin-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './admin.component';
+import { EmailBlastComponent } from './email-blast/email-blast.component';
+import { UserComponent } from './user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    // for the admin path load the components
+    path: 'admin', 
+    component: AdminComponent,
+    children: [
+      // to load the paths we must add router outlet in the admin component
+      { path: '', component: UserComponent },
+      { path: 'blast', component: EmailBlastComponent },
+    ]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
