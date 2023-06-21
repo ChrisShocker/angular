78bbce486837bb4b3f585b59e7f2baf9aab4594e
Christian Andersen <Christian.Andersen@dhsoha.state.or.us>
2023-06-21 16:10:50 -0700

    add email blast component

--- Angular-CLI/angular-routing/src/app/admin/admin.module.ts
+++ Angular-CLI/angular-routing/src/app/admin/admin.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
+import { EmailBlastComponent } from './email-blast/email-blast.component';
 
 
 @NgModule({
   declarations: [
-    AdminComponent
+    AdminComponent,
+    EmailBlastComponent
   ],
   imports: [
     CommonModule,
