a96651a043d61d161d8ca0a48fe172d3636c2352
ChrisAndersen <Christian.Andersen@dhsoha.state.or.us>
2023-06-26 15:17:21 -0700

    segment buttons to new comp add routing

--- Styling-Applications-with-Angular-Material/angularMaterial/src/app/demo/demo.module.ts
+++ Styling-Applications-with-Angular-Material/angularMaterial/src/app/demo/demo.module.ts
@@ -2,8 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DemoRoutingModule } from './demo-routing.module';
 import { ButtonsComponent } from './buttons/buttons.component';
+
+//import material module to access all of angular materials 
 import { MaterialModule } from '../shared/material.module';
 
+//adding form module since Angular Materials has it as a dependency
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +16,8 @@ import { MaterialModule } from '../shared/material.module';
   imports: [
     CommonModule,
     DemoRoutingModule,
-    MaterialModule
+    MaterialModule,
+    FormsModule
   ]
 })
 export class DemoModule { }
