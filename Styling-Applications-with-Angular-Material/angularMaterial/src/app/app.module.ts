18ca20cc9c2996d8e5b37750a2819c984163bf1b
ChrisAndersen <Christian.Andersen@dhsoha.state.or.us>
2023-06-26 14:15:51 -0700

    segment modules materialModule contains all imports for Angular Materials

--- Styling-Applications-with-Angular-Material/angularMaterial/src/app/app.module.ts
+++ Styling-Applications-with-Angular-Material/angularMaterial/src/app/app.module.ts
@@ -2,10 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-// angular materials website: https://material.angular.io/
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MaterialModule } from './shared/material.module';
 
 @NgModule({
   declarations: [
@@ -14,9 +11,7 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatButtonModule,
-    MatIconModule,
-    MatCheckboxModule
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
