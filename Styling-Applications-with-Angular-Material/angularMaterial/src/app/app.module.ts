21504de621a003adb7a7501924c53dd294948d6b
ChrisAndersen <Christian.Andersen@dhsoha.state.or.us>
2023-06-26 13:51:32 -0700

    add link to angular materials website

--- Styling-Applications-with-Angular-Material/angularMaterial/src/app/app.module.ts
+++ Styling-Applications-with-Angular-Material/angularMaterial/src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// angular materials website: https://material.angular.io/
 
 @NgModule({
   declarations: [
