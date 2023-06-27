ff2a675d4eff9086ca186dda13bf74c4706d1a05
ChrisAndersen <Christian.Andersen@dhsoha.state.or.us>
2023-06-27 12:14:39 -0700

    add components for contact-manager layout

--- Styling-Applications-with-Angular-Material/angularMaterial/src/app/contact-manager/contact-manager.module.ts
+++ Styling-Applications-with-Angular-Material/angularMaterial/src/app/contact-manager/contact-manager.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContactManagerAppComponent } from './contact-manager-app.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { MainContentComponent } from './components/main-content/main-content.component';
+import { SideNavComponent } from './components/side-nav/side-nav.component';
 
 
 
 @NgModule({
   declarations: [
-    ContactManagerAppComponent
+    ContactManagerAppComponent,
+    ToolbarComponent,
+    MainContentComponent,
+    SideNavComponent
   ],
   imports: [
     CommonModule
