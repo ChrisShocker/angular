ed4bf61c335e16672c51c45634d008a7ef2939ed
ChrisAndersen <Christian.Andersen@dhsoha.state.or.us>
2023-06-27 12:25:43 -0700

    add routing and imports

--- Styling-Applications-with-Angular-Material/angularMaterial/src/app/contact-manager/contact-manager.module.ts
+++ Styling-Applications-with-Angular-Material/angularMaterial/src/app/contact-manager/contact-manager.module.ts
@@ -4,8 +4,25 @@ import { ContactManagerAppComponent } from './contact-manager-app.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { MainContentComponent } from './components/main-content/main-content.component';
 import { SideNavComponent } from './components/side-nav/side-nav.component';
+//import material module to access all of angular materials 
+import { MaterialModule } from '../shared/material.module';
 
+//adding form module since Angular Materials has it as a dependency
+import { FormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { Routes, RouterModule } from '@angular/router';
 
+const routes: Routes = [
+  // use lazy loading for demo module
+  {
+    path: '', component: ContactManagerAppComponent,
+    children: [
+      { path: '', component: MainContentComponent }
+    ]
+
+  },
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   declarations: [
@@ -15,7 +32,11 @@ import { SideNavComponent } from './components/side-nav/side-nav.component';
     SideNavComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule,
+    FormsModule,
+    FlexLayoutModule,
+    RouterModule.forChild(routes)
   ]
 })
 export class ContactManagerModule { }
