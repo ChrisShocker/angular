c42f32b6f6f3b3ec12923f317ce8a9bd351277c9
ChrisAndersen <Christian.Andersen@dhsoha.state.or.us>
2023-06-27 15:55:39 -0700

    subscribe to changes

--- Styling-Applications-with-Angular-Material/angularMaterial/src/app/contact-manager/components/side-nav/side-nav.component.ts
+++ Styling-Applications-with-Angular-Material/angularMaterial/src/app/contact-manager/components/side-nav/side-nav.component.ts
@@ -1,4 +1,4 @@
-import { BreakpointObserver } from '@angular/cdk/layout';
+import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
 import { Component } from '@angular/core';
 
 //define custom breakpoint width
@@ -11,12 +11,15 @@ const SMALL_WIDTH_BREAKPOINT = 720;
 })
 export class SideNavComponent {
 
-  public isScreenSmall : boolean = false;
-  showFiller : boolean = false;
+  public isScreenSmall: boolean = false;
+  showFiller: boolean = false;
 
-  constructor(private breakpointObserver: BreakpointObserver){
+  constructor(private breakpointObserver: BreakpointObserver) {
     //we can use BreakpointObserver to watch changes in the dom and react to them with observe
     this.breakpointObserver.observe([`(max-width: ${SMALL_WIDTH_BREAKPOINT}px)`])
+      .subscribe((state: BreakpointState) => {
+        this.isScreenSmall = state.matches;
+      });
 
   }
 
