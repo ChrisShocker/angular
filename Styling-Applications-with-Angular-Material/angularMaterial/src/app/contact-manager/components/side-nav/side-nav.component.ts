2d8ef03ddc7ac11d04c9a0fdcf8f7829252c7f56
ChrisAndersen <Christian.Andersen@dhsoha.state.or.us>
2023-06-27 15:52:13 -0700

    add data binding with custom breakpoints

--- Styling-Applications-with-Angular-Material/angularMaterial/src/app/contact-manager/components/side-nav/side-nav.component.ts
+++ Styling-Applications-with-Angular-Material/angularMaterial/src/app/contact-manager/components/side-nav/side-nav.component.ts
@@ -1,4 +1,8 @@
+import { BreakpointObserver } from '@angular/cdk/layout';
 import { Component } from '@angular/core';
+
+//define custom breakpoint width
+const SMALL_WIDTH_BREAKPOINT = 720;
 
 @Component({
   selector: 'app-side-nav',
@@ -7,6 +11,13 @@ import { Component } from '@angular/core';
 })
 export class SideNavComponent {
 
+  public isScreenSmall : boolean = false;
   showFiller : boolean = false;
+
+  constructor(private breakpointObserver: BreakpointObserver){
+    //we can use BreakpointObserver to watch changes in the dom and react to them with observe
+    this.breakpointObserver.observe([`(max-width: ${SMALL_WIDTH_BREAKPOINT}px)`])
+
+  }
 
 }
