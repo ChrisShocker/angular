3d9b0f86ef774b92e1bb67ee55aa6810c74a8510
ChrisAndersen <Christian.Andersen@dhsoha.state.or.us>
2023-06-28 10:48:05 -0700

    build service initialize dataStore add data protection load and subscribe to API add getter function for users

--- Styling-Applications-with-Angular-Material/angularMaterial/src/app/contact-manager/services/user.service.ts
+++ Styling-Applications-with-Angular-Material/angularMaterial/src/app/contact-manager/services/user.service.ts
@@ -1,17 +1,43 @@
 import { Injectable } from '@angular/core';
 import { User } from '../models/user';
 import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
+  //prevent external components from manipulating our private data 
+  private _users: BehaviorSubject<User[]>;
+
+
+  //build data store
   private dataStore: {
     users: User[];
   }
 
-  constructor(private http: HttpClient) { 
-    this.dataStore = { users: []}
+  constructor(private http: HttpClient) {
+    //initialize dataStore, will hold objects retirieved from API
+    this.dataStore = { users: [] }
+    //initialize behaviorSubject
+    this._users = new BehaviorSubject<User[]>([]);
   }
+
+  loadUsersFromAPI() {
+    const userAPIURL = 'https://angular-material-api.azurewebsites.net/users';
+
+    //call API to fill dataStore user Object Array
+    return this.http.get<User[]>(userAPIURL)
+      .subscribe(data => {
+        this.dataStore.users = data;
+      }, error => {
+        console.log(`error:  ${error}`);
+      });
+  }
+
+  get users(): Observable<User[]> {
+    return this._users.asObservable();
+  }
+  
 }
