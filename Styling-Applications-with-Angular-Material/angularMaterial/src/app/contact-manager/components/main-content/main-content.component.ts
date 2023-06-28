4d5004deb59cfd806d59765c5ae467a14d8ee695
ChrisAndersen <Christian.Andersen@dhsoha.state.or.us>
2023-06-28 12:06:04 -0700

    build class

--- Styling-Applications-with-Angular-Material/angularMaterial/src/app/contact-manager/components/main-content/main-content.component.ts
+++ Styling-Applications-with-Angular-Material/angularMaterial/src/app/contact-manager/components/main-content/main-content.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from '../../services/user.service';
+import { User } from '../../models/user';
 
 @Component({
   selector: 'app-main-content',
   templateUrl: './main-content.component.html',
   styleUrls: ['./main-content.component.scss']
 })
-export class MainContentComponent {
+export class MainContentComponent implements OnInit {
+
+  user: User | undefined;
+
+  constructor(
+    //add ActivatedRoute to get route params
+    private route: ActivatedRoute, 
+    //add userService to lookup a user by ID
+    private service: UserService){
+
+  }
+
+  ngOnInit(): void {
+    //subscribe to route to get id params 
+    this.route.params.subscribe(params =>{
+      const id = params['id'];
+      //call userService function to get user by id
+      this.user = this.service.getUserById(id);
+      
+    })
+  }
 
 }
