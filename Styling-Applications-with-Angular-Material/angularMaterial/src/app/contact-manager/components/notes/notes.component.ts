3c431f8978b47a6acfd6d3fdc2ed941c76b719bd
ChrisAndersen <Christian.Andersen@dhsoha.state.or.us>
2023-06-29 09:07:20 -0700

    add data source for table

--- Styling-Applications-with-Angular-Material/angularMaterial/src/app/contact-manager/components/notes/notes.component.ts
+++ Styling-Applications-with-Angular-Material/angularMaterial/src/app/contact-manager/components/notes/notes.component.ts
@@ -1,15 +1,28 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Note } from '../../models/note';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-notes',
   templateUrl: './notes.component.html',
   styleUrls: ['./notes.component.scss']
 })
-export class NotesComponent {
-
+export class NotesComponent implements OnInit {
   @Input() notes: Note[] | undefined;
 
+  displayedColumns: string[] = ['position', 'title', 'date'];
+  dataSource: MatTableDataSource<Note>;
+
+  constructor(){
+    console.log("In Constructor ")
+    this.dataSource = new MatTableDataSource<Note>(this.notes);
+
+  }
+
+
+  ngOnInit(): void {
+    console.log("In init ")
+  }
 
 
 }
