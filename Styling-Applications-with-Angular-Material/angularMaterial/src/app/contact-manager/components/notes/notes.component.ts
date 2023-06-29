6812373bae625130962632184c37f614d973824d
ChrisAndersen <Christian.Andersen@dhsoha.state.or.us>
2023-06-29 09:33:17 -0700

    add pigination

--- Styling-Applications-with-Angular-Material/angularMaterial/src/app/contact-manager/components/notes/notes.component.ts
+++ Styling-Applications-with-Angular-Material/angularMaterial/src/app/contact-manager/components/notes/notes.component.ts
@@ -1,27 +1,32 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit, ViewChild } from '@angular/core';
 import { Note } from '../../models/note';
 import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-notes',
   templateUrl: './notes.component.html',
   styleUrls: ['./notes.component.scss']
 })
-export class NotesComponent implements OnInit {
+export class NotesComponent implements OnInit, AfterViewInit {
   @Input() notes: Note[] | undefined;
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
 
   displayedColumns: string[] = ['position', 'title', 'date'];
+
   dataSource: MatTableDataSource<Note>;
 
   constructor(){
     this.dataSource = new MatTableDataSource<Note>;
 
   }
-
-
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+  }
+  
   ngOnInit(): void {
      this.dataSource = new MatTableDataSource<Note>(this.notes);
   }
-
 
 }
