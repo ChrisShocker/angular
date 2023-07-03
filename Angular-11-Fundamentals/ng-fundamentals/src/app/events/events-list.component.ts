a3c8e209e05a4fdd8eccba78c61b1b15e1feb6de
ChrisAndersen <Christian.Andersen@dhsoha.state.or.us>
2023-07-03 13:16:03 -0700

    create temp object and display it

--- Angular-11-Fundamentals/ng-fundamentals/src/app/events/events-list.component.ts
+++ Angular-11-Fundamentals/ng-fundamentals/src/app/events/events-list.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class EventsListComponent {
 
+
+  //create temporary event object
+  event = {
+    id: 1,
+    name: "Angular Connect",
+    date: "9/26/2036",
+    time: "10:00 am",
+    price: 599.99,
+    imageUrl: "../assets/images/angularconnect-shield.png",
+    location: {
+      address: "1057 DT",
+      city: "London",
+      country: "England"
+    }
+  }
+
 }
