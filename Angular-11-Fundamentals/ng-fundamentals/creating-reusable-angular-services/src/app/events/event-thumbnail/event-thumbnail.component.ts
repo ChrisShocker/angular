956dab48d5b4cb55ddb46d1c8397880118fffd4c
ChrisAndersen <Christian.Andersen@dhsoha.state.or.us>
2023-07-05 11:54:54 -0700

    create folder for class module

---
+++ Angular-11-Fundamentals/ng-fundamentals/creating-reusable-angular-services/src/app/events/event-thumbnail/event-thumbnail.component.ts
@@ -0,0 +1,38 @@
+//Add input to allow component to expect a value to be passed in
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-event-thumbnail',
+  templateUrl: './event-thumbnail.component.html',
+  styleUrls: ['./event-thumbnail.component.css']
+})
+export class EventThumbnailComponent {
+  //create an event object for component to bind to
+  //add @input decorator to define the value will be passed in for the event (not js event)
+  @Input() event:any
+
+  // To pass an event/change to a parent component:
+  //  - @Output decorator must be used with an EventEmitter()
+  @Output() eventClick = new EventEmitter();
+
+  handleClickMe(): void{
+    //display click event
+    console.log("Button event triggered in child component: event-thumbnail");
+
+    //to pass the event to a parent component, the event must be emitted
+    //only one value can be emitted from an event, object can also be emitted
+    this.eventClick.emit(this.event.name);
+  }
+
+  logFoo() {
+    console.log("Called Child Component function logFoo()");
+  }
+
+  //multiple classes can be returned back to the class calling binding
+  //classes can be returned in an array or a string
+  getNameClasses(): string{
+    return "bold white";
+    //return ["bold", "white"];
+  }
+
+}
