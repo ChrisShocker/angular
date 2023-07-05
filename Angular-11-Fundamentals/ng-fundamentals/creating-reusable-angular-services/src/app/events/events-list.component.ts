40cc1149e802c776da43b187766df71cac3b3feb
ChrisAndersen <Christian.Andersen@dhsoha.state.or.us>
2023-07-05 13:06:31 -0700

    move event initi to OnInit

--- Angular-11-Fundamentals/ng-fundamentals/creating-reusable-angular-services/src/app/events/events-list.component.ts
+++ Angular-11-Fundamentals/ng-fundamentals/creating-reusable-angular-services/src/app/events/events-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { EventService } from './shared/event.service';
 
 @Component({
@@ -6,12 +6,11 @@ import { EventService } from './shared/event.service';
   templateUrl: './events-list.component.html',
   styleUrls: ['./events-list.component.css']
 })
-export class EventsListComponent {
-  events: any[];
+export class EventsListComponent implements OnInit{
+  events!: any[];
 
   // register/inject event service with events-list component
   constructor(private eventService: EventService){
-    this.events = this.eventService.getEvents();
   }
 
   //catcher for click events from child component
@@ -19,4 +18,7 @@ export class EventsListComponent {
     console.log("Button event triggerend in parent component events-list " + eventData);
   }
 
+  ngOnInit(){
+    this.events = this.eventService.getEvents();
+  }
 }
