4a6193fdecf446ab2aac11888a3c720eff32c9f0
ChrisAndersen <Christian.Andersen@dhsoha.state.or.us>
2023-07-06 11:10:18 -0700

    add example for pulling parameters from URL

--- Angular-11-Fundamentals/ng-fundamentals/6-routing-and-navigating-pages/src/app/events/event-details/event-details.component.ts
+++ Angular-11-Fundamentals/ng-fundamentals/6-routing-and-navigating-pages/src/app/events/event-details/event-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { EventService } from '../shared/event.service';
+import {ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-event-details',
@@ -11,11 +12,16 @@ export class EventDetailsComponent implements OnInit{
   event: any;
 
   //inject eventService
-  constructor(private eventService: EventService){
+  constructor(private eventService: EventService, private activatedRoute: ActivatedRoute){
   }
 
   ngOnInit(): void {
-    this.event = this.eventService.getEventById(1);
+    //instead of hard coding the url we can pass in the url parameter using activatedRoute
+    //this.event = this.eventService.getEventById(1);
+
+    //use the actual url parameter from activated route 
+    //note since the id isn't a string we must cast it to a number using '+'
+    this.event = this.eventService.getEventById(+this.activatedRoute.snapshot.params['id']);
   }
 
 
