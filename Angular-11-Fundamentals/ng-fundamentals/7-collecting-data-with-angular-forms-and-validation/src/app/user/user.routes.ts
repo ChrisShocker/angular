356cb7fa2c73b9f790aaa9174b2a884b49a43417
ChrisAndersen <Christian.Andersen@dhsoha.state.or.us>
2023-07-10 14:50:15 -0700

    add route for user login

--- Angular-11-Fundamentals/ng-fundamentals/7-collecting-data-with-angular-forms-and-validation/src/app/user/user.routes.ts
+++ Angular-11-Fundamentals/ng-fundamentals/7-collecting-data-with-angular-forms-and-validation/src/app/user/user.routes.ts
@@ -1,5 +1,7 @@
+import { LoginComponent } from "./login/login.component";
 import { ProfileComponent } from "./profile/profile.component";
 
 export const userRoutes = [
+    {path: 'login', component: LoginComponent},
     {path: 'profile', component: ProfileComponent}
 ]
