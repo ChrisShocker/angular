75aac95c4e03cafe5b28c36eb18ce7c27e88ba13
ChrisAndersen <Christian.Andersen@dhsoha.state.or.us>
2023-07-10 13:35:13 -0700

    create folder for class section

---
+++ Angular-11-Fundamentals/ng-fundamentals/7-collecting-data-with-angular-forms-and-validation/src/app/events/create-event/create-event.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-create-event',
+  templateUrl: './create-event.component.html',
+  styleUrls: ['./create-event.component.css']
+})
+export class CreateEventComponent {
+
+  isDirty:boolean = true;
+
+  constructor(private router: Router){
+  }
+
+  cancelButton(){
+    this.router.navigate(['/events']);
+  }
+}
