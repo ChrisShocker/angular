a10011e1981b07ce46d0723a1bf32cbf5eb858ed
ChrisAndersen <Christian.Andersen@dhsoha.state.or.us>
2023-07-10 16:04:54 -0700

    navigate user to events post login

--- Angular-11-Fundamentals/ng-fundamentals/7-collecting-data-with-angular-forms-and-validation/src/app/user/login/login.component.ts
+++ Angular-11-Fundamentals/ng-fundamentals/7-collecting-data-with-angular-forms-and-validation/src/app/user/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,11 +11,18 @@ export class LoginComponent {
   userName: string = 'user';
   password: string = 'password';
 
-  constructor(private authService: AuthService){
+  constructor(private authService: AuthService, private router: Router){
   }
 
 
   login(formValues: any){
+    //authenticate user with from vallues passed in 
     this.authService.loginUser(formValues.userName, formValues.password);
+    //once authenticated route the user to the events page
+    this.router.navigate(['events']);
+  }
+
+  cancel(){
+    this.router.navigate(['events']);
   }
 }
