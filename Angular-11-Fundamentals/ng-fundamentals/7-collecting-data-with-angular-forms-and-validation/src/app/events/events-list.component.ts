da057ed33b03ba935e8c4c2c8cc827112cc8b766
ChrisAndersen <Christian.Andersen@dhsoha.state.or.us>
2023-07-10 14:38:03 -0700

    add types to existing variables

--- Angular-11-Fundamentals/ng-fundamentals/7-collecting-data-with-angular-forms-and-validation/src/app/events/events-list.component.ts
+++ Angular-11-Fundamentals/ng-fundamentals/7-collecting-data-with-angular-forms-and-validation/src/app/events/events-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { EventService } from './shared/event.service';
 import { ToastrService } from '../common/toastr.service';
 import { ActivatedRoute, Route } from '@angular/router';
+import { IEvent } from './shared';
 
 declare let toastr: { success: (arg0: any) => void; };
 
@@ -11,7 +12,7 @@ declare let toastr: { success: (arg0: any) => void; };
   styleUrls: ['./events-list.component.css']
 })
 export class EventsListComponent implements OnInit{
-  events!: any;
+  events!: IEvent[];
 
   // register/inject event and toastr services with events-list component
   constructor(private eventService: EventService, private toasterService: ToastrService, private activatedRoute: ActivatedRoute){
