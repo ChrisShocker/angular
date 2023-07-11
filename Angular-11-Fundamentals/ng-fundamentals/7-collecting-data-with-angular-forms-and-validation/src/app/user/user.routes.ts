e39ed21ec76e7a8da910a3b60f92649ba5b59607
ChrisAndersen <Christian.Andersen@dhsoha.state.or.us>
2023-07-11 10:53:10 -0700

    update existing form

--- Angular-11-Fundamentals/ng-fundamentals/7-collecting-data-with-angular-forms-and-validation/src/app/user/user.routes.ts
+++ Angular-11-Fundamentals/ng-fundamentals/7-collecting-data-with-angular-forms-and-validation/src/app/user/user.routes.ts
@@ -1,9 +1,7 @@
 import { LoginComponent } from "./login/login.component";
 import { ProfileComponent } from "./profile/profile.component";
-import { ProfileReactiveFormComponent } from "./profile-reactive-form/profile-reactive-form.component";
 
 export const userRoutes = [
     {path: 'login', component: LoginComponent},
-    {path: 'profile/reactive', component: ProfileReactiveFormComponent},
     {path: 'profile', component: ProfileComponent}
 ]
