9a490e30fe807d58e8c30c66d5b6842a2b31c08a
ChrisAndersen <Christian.Andersen@dhsoha.state.or.us>
2023-07-11 11:25:40 -0700

    add reactive bindings for validation

--- Angular-11-Fundamentals/ng-fundamentals/7-collecting-data-with-angular-forms-and-validation/src/app/user/profile/profile.component.ts
+++ Angular-11-Fundamentals/ng-fundamentals/7-collecting-data-with-angular-forms-and-validation/src/app/user/profile/profile.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
   // this form uses reactive form validation
 
+  profileForm!: FormGroup;
+
+  constructor() {}
+
+  ngOnInit() {
+    //create form control for each input on form
+    let firstName = new FormControl();
+    let lastName = new FormControl();
+
+    //controls must be added to a form group
+    this.profileForm = new FormGroup({
+      //set propertirs for the form group
+      firstName: firstName,
+      lastName: lastName,
+    });
+  }
 }
