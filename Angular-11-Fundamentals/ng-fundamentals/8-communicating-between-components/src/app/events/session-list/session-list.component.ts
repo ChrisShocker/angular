3f2125c6d1eddf98a0b5c38ef39427e7ecb586ff
ChrisAndersen <Christian.Andersen@dhsoha.state.or.us>
2023-07-20 08:51:06 -0700

    build component

--- Angular-11-Fundamentals/ng-fundamentals/8-communicating-between-components/src/app/events/session-list/session-list.component.ts
+++ Angular-11-Fundamentals/ng-fundamentals/8-communicating-between-components/src/app/events/session-list/session-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ISession } from '../shared';
 
 @Component({
   selector: 'app-session-list',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./session-list.component.css']
 })
 export class SessionListComponent {
+  @Input() sessions!: ISession[];
+
+  constructor(){
+  }
 
 }
