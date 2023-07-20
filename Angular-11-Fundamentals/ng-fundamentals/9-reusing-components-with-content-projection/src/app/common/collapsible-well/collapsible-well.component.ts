19a962f6ef9bf6a6d23d7325adf78944ace06254
ChrisAndersen <Christian.Andersen@dhsoha.state.or.us>
2023-07-20 11:36:18 -0700

    add well for content projection example

--- Angular-11-Fundamentals/ng-fundamentals/9-reusing-components-with-content-projection/src/app/common/collapsible-well/collapsible-well.component.ts
+++ Angular-11-Fundamentals/ng-fundamentals/9-reusing-components-with-content-projection/src/app/common/collapsible-well/collapsible-well.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-collapsible-well',
   templateUrl: './collapsible-well.component.html',
-  styleUrls: ['./collapsible-well.component.css']
+  styleUrls: ['./collapsible-well.component.css'],
 })
 export class CollapsibleWellComponent {
+  @Input() title!: string;
 
+  //toggle to display well content
+  isVisible: boolean = true;
+  toggleContent() {
+    this.isVisible = !this.isVisible;
+  }
 }
