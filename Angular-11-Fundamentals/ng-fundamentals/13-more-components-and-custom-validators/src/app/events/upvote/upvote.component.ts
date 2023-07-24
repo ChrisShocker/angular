3f830767794e203d8a1849814680e0adc2138432
Christian Andersen <Christian.Andersen@dhsoha.state.or.us>
2023-07-24 13:39:05 -0700

    build component to allow users to upvote sessions

--- Angular-11-Fundamentals/ng-fundamentals/13-more-components-and-custom-validators/src/app/events/upvote/upvote.component.ts
+++ Angular-11-Fundamentals/ng-fundamentals/13-more-components-and-custom-validators/src/app/events/upvote/upvote.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-upvote',
   templateUrl: './upvote.component.html',
-  styleUrls: ['./upvote.component.css']
+  styleUrls: ['./upvote.component.css'],
 })
 export class UpvoteComponent {
+  // number of votes
+  @Input() count: number = 0;
+  // if user voted
+  @Input() voted: boolean = false;
+  // vote event
+  @Output() vote = new EventEmitter();
 
+
+  onClick(){
+    //when clicked emit vote event
+    this.vote.emit({});
+  }
 }
