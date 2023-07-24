61ec1756f4a721d2903270d8ab3c9f5ba790fcbf
Christian Andersen <Christian.Andersen@dhsoha.state.or.us>
2023-07-24 14:39:18 -0700

    add custom validaor to angulars validator list

--- Angular-11-Fundamentals/ng-fundamentals/13-more-components-and-custom-validators/src/app/events/create-event/location-validator.directive.ts
+++ Angular-11-Fundamentals/ng-fundamentals/13-more-components-and-custom-validators/src/app/events/create-event/location-validator.directive.ts
@@ -1,19 +1,18 @@
-import { Directive } from '@angular/core'; import {
-  AbstractControl,
-  FormGroup,
-  ValidationErrors,
-  Validator,
-  Validators,
-} from '@angular/forms';
+import { Directive } from '@angular/core';
+//Note to add a custom validator to angular we must add it to the NG_Validators service
+import { FormGroup, Validator, NG_VALIDATORS } from '@angular/forms';
 
 @Directive({
   selector: '[validateLocation]',
+  // add custom validator to the NG_Validators service
+  // note the multi parameter, this adds our validator to existing services in NG_VALIDATORS,
+  // without removeing all the existing ones
+  providers: [{provide: NG_VALIDATORS, useExisting: LocationValidator, multi: true}]
 })
 
 //build a validator for checking the location values on the create-event form
 export class LocationValidator implements Validator {
-
-  validate(formGroup: FormGroup): { [key: string]: any} {
+  validate(formGroup: FormGroup): { [key: string]: any } {
     //access address using indexor and target the DOM formGroup
     let addressControl = formGroup.controls['address'];
     let cityControl = formGroup.controls['city'];
