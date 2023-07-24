90261a7851d6ad65a1f6ac2088832578e1e6b54f
Christian Andersen <Christian.Andersen@dhsoha.state.or.us>
2023-07-24 11:41:28 -0700

    auto close modal on click using ViewChild

--- Angular-11-Fundamentals/ng-fundamentals/12-creating-directives-and-advanced-components/src/app/common/simple-modal/simple-modal.component.ts
+++ Angular-11-Fundamentals/ng-fundamentals/12-creating-directives-and-advanced-components/src/app/common/simple-modal/simple-modal.component.ts
@@ -1,13 +1,23 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Inject, Input, ViewChild } from '@angular/core';
+import { JQ_TOKEN } from '../jQuery.service';
 
 @Component({
   selector: 'app-simple-modal',
   templateUrl: './simple-modal.component.html',
-  styleUrls: ['./simple-modal.component.css']
+  styleUrls: ['./simple-modal.component.css'],
 })
 export class SimpleModalComponent {
-@Input() title: string = '';
-@Input() elementId: string = '';
+  @Input() title: string = '';
+  @Input() elementId: string = '';
+  //use ViewChild to give a reference to the DOM node
+  //ViewCildren could be used for a list inside an ngFor
+  @ViewChild('modalContainer') containerElement!: ElementRef;
 
 
+  constructor(@Inject(JQ_TOKEN) private $: any){}
+
+  closeModal(){
+    //call Jquery method to hide the modal
+    this.$(this.containerElement.nativeElement).modal('hide');
+  }
 }
