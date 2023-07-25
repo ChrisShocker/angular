6797207e07ce98efaf3eac1d016414c96862df46
Christian Andersen <Christian.Andersen@dhsoha.state.or.us>
2023-07-25 14:36:11 -0700

    convert user login to http call

--- Angular-11-Fundamentals/ng-fundamentals/14-communicating-with-server-using-http-observables-rx/src/app/user/auth.service.ts
+++ Angular-11-Fundamentals/ng-fundamentals/14-communicating-with-server-using-http-observables-rx/src/app/user/auth.service.ts
@@ -1,30 +1,59 @@
 import { Injectable } from '@angular/core';
 import { IUser } from './user.model';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, of, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor() {}
-
   currentUser!: IUser;
 
+  constructor(private http: HttpClient) {}
+
+  //get user info from server and set current user to it
   loginUser(userName: string, password: string) {
-    //hard code a user for now
-    this.currentUser = {
-      id: 1,
-      userName: 'userName',
-      firstName: 'John',
-      lastName: 'Papa'
+    // //hard code a user for now
+    // this.currentUser = {
+    //   id: 1,
+    //   userName: 'userName',
+    //   firstName: 'John',
+    //   lastName: 'Papa'
+    // };
+
+    let url: string = '/api/login';
+    let options: {} = {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
     };
+    let body: {} = { username: userName, password: password };
+
+    //use tap pipe to see data that comes back and act on it
+    return (
+      this.http
+        .post(url, body, options)
+        .pipe(
+          tap((data) => {
+            let someObj: any = data;
+            const field = 'user';
+            console.log(data);
+            this.currentUser = someObj[field as keyof IUser];
+          })
+        )
+        // if an error occurs return an observable of false
+        .pipe(
+          catchError((err) => {
+            return of(false);
+          })
+        )
+    );
   }
 
   //fake an isAuthenticated call
-  isAuthenticated(){
+  isAuthenticated() {
     return !!this.currentUser;
   }
 
-  updateCurrentUser(firstName: string, lastName: string){
+  updateCurrentUser(firstName: string, lastName: string) {
     this.currentUser.firstName = firstName;
     this.currentUser.lastName = lastName;
   }
--- Angular-11-Fundamentals/ng-fundamentals/14-communicating-with-server-using-http-observables-rx/src/app/user/login/login.component.ts
+++ Angular-11-Fundamentals/ng-fundamentals/14-communicating-with-server-using-http-observables-rx/src/app/user/login/login.component.ts
@@ -5,27 +5,33 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-// This component uses template form validation 
+  // This component uses template form validation
 
   userName: string = 'user';
   password: string = 'password';
   mouseOverLogin!: boolean;
+  loginInvalid: boolean = false;
 
-  constructor(private authService: AuthService, private router: Router){
-  }
+  constructor(private authService: AuthService, private router: Router) {}
 
-
-  login(formValues: any){
-    //authenticate user with from vallues passed in 
-    this.authService.loginUser(formValues.userName, formValues.password);
+  login(formValues: any) {
+    //authenticate user with from vallues passed in
+    this.authService
+      .loginUser(formValues.userName, formValues.password)
+      .subscribe((resp) => {
+        if (!resp) {
+          this.loginInvalid = true;
+        } else {
+          this.router.navigate(['events']);
+        }
+      });
     //once authenticated route the user to the events page
-    this.router.navigate(['events']);
   }
 
-  cancel(){
+  cancel() {
     this.router.navigate(['events']);
   }
 }
