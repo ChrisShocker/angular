acd94d539167f3ee0c2a042431dc68e70689d822
Christian Andersen <Christian.Andersen@dhsoha.state.or.us>
2023-07-28 12:18:12 -0700

    add routing for feature module

--- Angular-Routing/APM-Start v14/src/app/products/product.module.ts
+++ Angular-Routing/APM-Start v14/src/app/products/product.module.ts
@@ -5,15 +5,18 @@ import { ProductDetailComponent } from './product-detail.component';
 import { ProductEditComponent } from './product-edit/product-edit.component';
 
 import { SharedModule } from '../shared/shared.module';
+import { RouterModule, Routes } from '@angular/router';
 
+let routes: Routes = [
+  { path: 'product', component: ProductListComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'product' },
+];
 @NgModule({
-  imports: [
-    SharedModule
-  ],
+  imports: [SharedModule, RouterModule.forChild(routes)],
   declarations: [
     ProductListComponent,
     ProductDetailComponent,
-    ProductEditComponent
-  ]
+    ProductEditComponent,
+  ],
 })
-export class ProductModule { }
+export class ProductModule {}
