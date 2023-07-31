2e0500ec9c134057b20a5104d85db8620a3f7466
ChrisAndersen <Christian.Andersen@dhsoha.state.or.us>
2023-07-31 16:19:47 -0700

    create and build resolver service

--- Angular-Routing/APM-Start v14/src/app/products/product-resolver.service.ts
+++ Angular-Routing/APM-Start v14/src/app/products/product-resolver.service.ts
@@ -1,22 +1,51 @@
 import { Injectable } from "@angular/core";
 import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from "@angular/router";
 
-import { Observable } from "rxjs";
+import { catchError, map, Observable, of } from "rxjs";
 
-import { Product } from "./product";
+import { ProductResolved } from "./product";
 import { ProductService } from "./product.service";
 
 @Injectable({
   providedIn: 'root'
 })
-export class ProductResolver implements Resolve<Product>{
+export class ProductResolver implements Resolve<ProductResolved>{
   constructor(private productService: ProductService) { }
 
   // resolver doesn't have to be subscribed to and won't
   // return until the data is fetched and subscription is complete
-  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Product>
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<ProductResolved>
   {
-    const id = Number(route.paramMap.get('id'));
-    return this.productService.getProduct(id);
+    const id = route.paramMap.get('id');
+
+    //catch id null
+    if (!id)
+    {
+      const message = `Product is null: ${id}`;
+      console.error(message);
+      //cast object as an observable using 'of'
+      return of({ product: null, error: message });
+    }
+
+    //catch error if id isn't a number
+    if (isNaN(+id))
+    {
+      const message = `Product id was not a number: ${id}`;
+      console.error(message);
+      //cast object as an observable using 'of'
+      return of({ product: null, error: message });
+    }
+
+    // catch and return all other errors
+    return this.productService.getProduct(+id)
+      .pipe(
+        map(product => ({ product: product })),
+        catchError(error =>
+        {
+          const message = `Retrieval error: ${error}`;
+          console.error(message);
+          return of({ product: null, error: message });
+        })
+      );
   }
 }
