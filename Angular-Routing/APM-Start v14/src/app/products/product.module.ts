b77053dfb51e2db31961766a336c90970e881f27
ChrisAndersen <Christian.Andersen@dhsoha.state.or.us>
2023-08-01 10:34:33 -0700

    add child routes for edit path

--- Angular-Routing/APM-Start v14/src/app/products/product.module.ts
+++ Angular-Routing/APM-Start v14/src/app/products/product.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 
 import { ProductListComponent } from './product-list.component';
 import { ProductDetailComponent } from './product-detail.component';
@@ -7,12 +7,31 @@ import { ProductEditComponent } from './product-edit/product-edit.component';
 import { SharedModule } from '../shared/shared.module';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductResolver } from './product-resolver.service';
+import { ProductEditInfoComponent } from './product-edit/product-edit-info.component';
+import { ProductEditTagsComponent } from './product-edit/product-edit-tags.component';
 
 let routes: Routes = [
   { path: 'products', component: ProductListComponent },
   //placeholders can be added to routes to pass info between components
   { path: 'products/:id', component: ProductDetailComponent, resolve: { resolvedData: ProductResolver } },
-  { path: 'products/:id/edit', component: ProductEditComponent, resolve: { resolvedData: ProductResolver } },
+  {
+    path: 'products/:id/edit', component: ProductEditComponent, resolve: { resolvedData: ProductResolver },
+    children: [
+      {
+        path: '',
+        redirectTo: 'info',
+        pathMatch: 'full'
+      },
+      {
+        path: 'info',
+        component: ProductEditInfoComponent
+      },
+      {
+        path: 'tags',
+        component: ProductEditTagsComponent
+      }
+    ]
+  },
   { path: '', pathMatch: 'full', redirectTo: 'products' },
 ];
 @NgModule({
@@ -21,6 +40,8 @@ let routes: Routes = [
     ProductListComponent,
     ProductDetailComponent,
     ProductEditComponent,
+    ProductEditInfoComponent,
+    ProductEditTagsComponent
   ],
 })
 export class ProductModule { }
