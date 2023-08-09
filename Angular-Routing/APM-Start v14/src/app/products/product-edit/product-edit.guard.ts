51bbd3f6aea792df8aa90088fbaf351048cc2630
ChrisAndersen <Christian.Andersen@dhsoha.state.or.us>
2023-08-09 13:31:08 -0700

    create guard shell

---
+++ Angular-Routing/APM-Start v14/src/app/products/product-edit/product-edit.guard.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanDeactivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { ProductEditComponent } from './product-edit.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+// note CanDeactivate requies a generic parameter
+export class ProductEditGuard implements CanDeactivate<ProductEditComponent> {
+  canDeactivate(
+    // component must match the generic parameter
+    component: ProductEditComponent,
+    currentRoute: ActivatedRouteSnapshot,
+    currentState: RouterStateSnapshot,
+    nextState?: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree
+  {
+    return true;
+  }
+
+}
