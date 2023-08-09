6cb1e315ec2510bad3d401a197025f85a2884451
ChrisAndersen <Christian.Andersen@dhsoha.state.or.us>
2023-08-09 08:46:09 -0700

    add loading spinner

--- Angular-Routing/APM-Start v14/src/app/app.component.ts
+++ Angular-Routing/APM-Start v14/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, Event, NavigationStart, NavigationEnd, NavigationError, NavigationCancel } from '@angular/router';
 import { slideInAnimation } from './app.animation';
 
 import { AuthService } from './user/auth.service';
@@ -13,6 +13,29 @@ import { AuthService } from './user/auth.service';
 export class AppComponent
 {
   pageTitle = 'Acme Product Management';
+  // variable to turn on loading spinner
+  isLoading: boolean = true;
+
+  constructor(private authService: AuthService, private router: Router)
+  {
+    router.events.subscribe((routerEvent: Event) =>
+    {
+      this.checkRouterEvent(routerEvent);
+    })
+  }
+
+  // checking routing events and enable/disable loading spinner
+  checkRouterEvent(routerEvent: Event): void
+  {
+    if (routerEvent instanceof NavigationStart)
+    {
+      this.isLoading = true;
+    }
+    if (routerEvent instanceof NavigationEnd || routerEvent instanceof NavigationCancel || routerEvent instanceof NavigationError)
+    {
+      this.isLoading = false;
+    }
+  }
 
   get isLoggedIn(): boolean
   {
@@ -27,8 +50,6 @@ export class AppComponent
     }
     return '';
   }
-
-  constructor(private authService: AuthService, private router: Router) { }
 
   logOut(): void
   {
