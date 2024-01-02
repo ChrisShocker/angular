c9122e938cc2c612d983e798a6992b5b17d860e3
Christian Andersen <Christian.Andersen@dhsoha.state.or.us>
2024-01-02 11:56:37 -0800

    add exmple and comments

--- Angular-RxJS-Reactive-Development/src/app/app.component.ts
+++ Angular-RxJS-Reactive-Development/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { of, from } from 'rxjs';
+import { of, from, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,18 +10,33 @@ export class AppComponent implements OnInit {
   title = 'Angular-RxJS-Reactive-Development';
 
   ngOnInit(): void {
+    /*
+     example RxJS functions
+      - note they call unsubscribe automatically
+    */
+    console.log('of example');
     of(2, 4, 6, 8).subscribe((data) => {
       console.log(data);
     });
 
+    console.log('from example');
     from([3, 6, 9, 12]).subscribe((data) => {
       console.log(data);
     });
 
+    console.log('from example with complete');
     from([4, 8, 12, 16]).subscribe({
       next: (data) => console.log(data),
       error: (error) => console.log(error),
       complete: () => console.log('Complete!'),
     });
+
+    // example map usage
+    console.log('map example');
+    of(1, 2, 3, 4)
+      .pipe(map((data) => data * 2))
+      .subscribe((data) => {
+        console.log(data);
+      });
   }
 }
