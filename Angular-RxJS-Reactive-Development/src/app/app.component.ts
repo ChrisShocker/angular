d5f4bb1de5d3f2b180d961126af33d6c8e9e2418
Christian Andersen <Christian.Andersen@dhsoha.state.or.us>
2024-01-02 11:23:14 -0800

    add example from and of rxjs functions

--- Angular-RxJS-Reactive-Development/src/app/app.component.ts
+++ Angular-RxJS-Reactive-Development/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { of, from } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Angular-RxJS-Reactive-Development';
+
+  ngOnInit(): void {
+    of(2, 4, 6, 8).subscribe((data) => {
+      console.log(data);
+    });
+    from([3, 6, 9, 12]).subscribe((data) => {
+      console.log(data);
+    });
+  }
 }
