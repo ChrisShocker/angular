73725e9dbb7a1d6051d329c943ff5589aa61593c
Christian Andersen <Christian.Andersen@dhsoha.state.or.us>
2024-01-02 11:25:38 -0800

    add from example wtih observerable methods

--- Angular-RxJS-Reactive-Development/src/app/app.component.ts
+++ Angular-RxJS-Reactive-Development/src/app/app.component.ts
@@ -13,8 +13,15 @@ export class AppComponent implements OnInit {
     of(2, 4, 6, 8).subscribe((data) => {
       console.log(data);
     });
+
     from([3, 6, 9, 12]).subscribe((data) => {
       console.log(data);
+    });
+
+    from([4, 8, 12, 16]).subscribe( {
+      next: (data) => console.log(data),
+      error: (error) => console.log(error),
+      complete: () => console.log('Complete!');
     });
   }
 }
