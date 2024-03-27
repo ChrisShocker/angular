eebd12ff99e2b13108794a399851d122db93aeed
Chris Andersen <Chris_Andersen@outlook.com>
2024-03-27 11:36:10 -0700

    update stream to correct service call to return product categories

--- Angular-RxJS-Reactive-Development/src/app/products/product-list-alt/product-list-alt.component.ts
+++ Angular-RxJS-Reactive-Development/src/app/products/product-list-alt/product-list-alt.component.ts
@@ -11,7 +11,7 @@ export class ProductListAltComponent {
   pageTitle = 'Products';
   errorMessage = '';
 
-  products$ = this.productService.products$.pipe(
+  products$ = this.productService.productWithCategories$.pipe(
     catchError((err) => {
       this.errorMessage = err;
       return EMPTY;
