0b6560572a0ac32f2a0b028b424520ce122a5318
Chris Andersen <Chris_Andersen@outlook.com>
2024-05-24 11:12:41 -0700

    add comments

--- Secure-Coding-in-Angular/04-sessions/session-app/src/app/message.service.ts
+++ Secure-Coding-in-Angular/04-sessions/session-app/src/app/message.service.ts
@@ -4,24 +4,25 @@ import { map, Observable } from 'rxjs';
 import { Message } from './message';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MessageService {
   private readonly SERVER_URL = '/api/messages';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getMessages(): Observable<Message[]> {
-    return this.http.get<Message[]>(this.SERVER_URL).pipe(
-      map(res => res || [])
-    );
+    return this.http
+      .get<Message[]>(this.SERVER_URL)
+      .pipe(map((res) => res || []));
   }
 
+  // includes XSRF token in post request
   addMessage(msg: Message): Observable<Message[]> {
-    return this.http.post<Message[]>(this.SERVER_URL, msg, {
-      headers: new HttpHeaders().set('Content-Type', 'application/json')
-    }).pipe(
-      map(res => res || [])
-    );
+    return this.http
+      .post<Message[]>(this.SERVER_URL, msg, {
+        headers: new HttpHeaders().set('Content-Type', 'application/json'),
+      })
+      .pipe(map((res) => res || []));
   }
 }
--- Secure-Coding-in-Angular/04-sessions/session-server/server.js
+++ Secure-Coding-in-Angular/04-sessions/session-server/server.js
@@ -1,3 +1,5 @@
+// Server handles the XSRF cookie protection and generates the XSRF token cookie
+
 const express = require('express')
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
@@ -10,13 +12,22 @@ const messages = [{
 }];
 
 app
-.use(cookieParser())
-.use(bodyParser.json())
-.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
+  .use(cookieParser())
+  .use(bodyParser.json())
+  .listen(port, () =>
+  {
+    console.log(`Listening on port ${port}`);
+  });
 
-app.get('/api/xsrfEndpoint', (req, res, next) => {
+// generate the XSRF token cookie and send to the FrontEnd
+// Do Not use this in production, this is just for the demo
+/**
+ * sameSite: 'Strict' - this will prevent the browser from sending the cookie in a cross-site request (e.g. from an external website)
+ * cookie can only pass in a 1st party context
+ * CORS isn't enabled
+ */
+app.get('/api/xsrfEndpoint', (req, res, next) =>
+{
   const csrfTokenSecret = 'top secret';
   res.cookie('XSRF-TOKEN', csrfTokenSecret, { httpOnly: false, sameSite: 'strict' });
   res.json({});
@@ -24,7 +35,8 @@ app.get('/api/xsrfEndpoint', (req, res, next) => {
 
 app.get('/api/messages', (_, res) => res.send(messages));
 
-app.post('/api/messages', (req, res) => {
+app.post('/api/messages', (req, res) =>
+{
   // should verify the CSRF Token cookie!
   const newMsg = req.body;
   messages.push(newMsg);
