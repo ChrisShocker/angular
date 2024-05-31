4e02fae51970bc3fffe2c10478639d8d90e1066c
Chris Andersen <Chris_Andersen@outlook.com>
2024-05-31 13:36:50 -0700

    add comments

--- Secure-Coding-in-Angular/07-conditional-views/src/app/directives/admin.directive.ts
+++ Secure-Coding-in-Angular/07-conditional-views/src/app/directives/admin.directive.ts
@@ -2,12 +2,16 @@ import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
 import { ROLE } from '../user.service';
 
 @Directive({
-    selector: '[appAdmin]',
-    standalone: true
+  selector: '[appAdmin]',
+  standalone: true,
 })
 export class AdminDirective {
-  constructor(private templateRef: TemplateRef<any>, private viewContainer: ViewContainerRef) { }
+  constructor(
+    private templateRef: TemplateRef<any>,
+    private viewContainer: ViewContainerRef
+  ) {}
 
+  // checks if the user has the role admin and displays the view if true
   @Input() set appAdmin(role: ROLE) {
     if (role === 'admin') {
       this.viewContainer.createEmbeddedView(this.templateRef);
