e64978551f7929857026c86d68423d42a5defe5f
Chris Andersen <Chris_Andersen@outlook.com>
2024-05-31 13:39:42 -0700

    add comments

--- Secure-Coding-in-Angular/07-conditional-views/src/app/directives/has-role.directive.ts
+++ Secure-Coding-in-Angular/07-conditional-views/src/app/directives/has-role.directive.ts
@@ -1,26 +1,42 @@
-import { Directive, Input, OnChanges, SimpleChanges, TemplateRef, ViewContainerRef } from '@angular/core';
+import {
+  Directive,
+  Input,
+  OnChanges,
+  SimpleChanges,
+  TemplateRef,
+  ViewContainerRef,
+} from '@angular/core';
 import { ROLE } from '../user.service';
 
+// more complex directive that has two inputs
 @Directive({
-    selector: '[appHasRole]',
-    standalone: true
+  selector: '[appHasRole]',
+  standalone: true,
 })
 export class HasRoleDirective implements OnChanges {
-  constructor(private templateRef: TemplateRef<any>, private viewContainer: ViewContainerRef) { }
+  constructor(
+    private templateRef: TemplateRef<any>,
+    private viewContainer: ViewContainerRef
+  ) {}
 
   private role!: ROLE;
   private userRole: ROLE = 'enduser';
 
+  // checks if the user has a role
   @Input() set appHasRole(role: ROLE) {
     this.role = role; // currently assumes this remains static
   }
 
+  // check for specific user role
   @Input() set appHasRoleUserRole(userRole: ROLE) {
     this.userRole = userRole;
   }
 
   ngOnChanges(changes: SimpleChanges): void {
-    if (changes['appHasRoleUserRole'].currentValue !== changes['appHasRoleUserRole'].previousValue) {
+    if (
+      changes['appHasRoleUserRole'].currentValue !==
+      changes['appHasRoleUserRole'].previousValue
+    ) {
       if (this.role === this.userRole) {
         this.viewContainer.createEmbeddedView(this.templateRef);
       } else {
