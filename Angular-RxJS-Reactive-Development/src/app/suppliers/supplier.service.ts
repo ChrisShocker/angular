d6c88d80a11b94372f398e310276bb9253460ab6
Chris Andersen <Chris_Andersen@outlook.com>
2024-06-21 10:33:04 -0700

    add bad practice manual inner observable subscription add good practice higher-order mapping obserbavle concatMap

--- Angular-RxJS-Reactive-Development/src/app/suppliers/supplier.service.ts
+++ Angular-RxJS-Reactive-Development/src/app/suppliers/supplier.service.ts
@@ -1,15 +1,42 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
-import { throwError, Observable } from 'rxjs';
+import { throwError, Observable, map, of, concat, tap, concatMap } from 'rxjs';
+import { Supplier } from './supplier';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SupplierService {
   suppliersUrl = 'api/suppliers';
 
-  constructor(private http: HttpClient) { }
+  // // mock observale to return a list of supplier ids
+  // // inner observable emits a supplier
+  // // THIS IS BAD PRACTICE
+  // suppliersWithMap$ = of(1, 5, 8).pipe(
+  //   map((id) => this.http.get<Supplier>(`${this.suppliersUrl}/${id}`))
+  // );
+
+  // use concatMap, a higher order rxjs operator to flatten the inner observable
+  // concatMap will subscribe and wait to emit results in sequence
+  suppliersWithConcatMap$ = of(1, 5, 8).pipe(
+    tap((id) => console.log('concatMap source Observable', id)),
+    concatMap((id) => this.http.get<Supplier>(`${this.suppliersUrl}/${id}`))
+  );
+
+  constructor(private http: HttpClient) {
+    // // manually subscribe to the observable with a nested observable subscription
+    // // THIS IS BAD PRACTICE
+    // this.suppliersWithMap$.subscribe((item) =>
+    //   item.subscribe((item) => console.log('map result ', item))
+    // );
+
+    // subsribe and log the results of concatMap
+    // Notice they will be console logged in order
+    this.suppliersWithConcatMap$.subscribe((item) =>
+      console.log('concatMap result ', item)
+    );
+  }
 
   private handleError(err: HttpErrorResponse): Observable<never> {
     // in a real world app, we may send the server to some remote logging infrastructure
@@ -26,5 +53,4 @@ export class SupplierService {
     console.error(err);
     return throwError(() => errorMessage);
   }
-
 }
