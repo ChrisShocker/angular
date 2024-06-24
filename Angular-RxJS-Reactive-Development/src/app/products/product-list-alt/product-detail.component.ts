897f5b6d337d90dc5e4f8efa52959622c1295bcd
Chris Andersen <Chris_Andersen@outlook.com>
2024-06-24 11:54:11 -0700

    combine all product details data streams

--- Angular-RxJS-Reactive-Development/src/app/products/product-list-alt/product-detail.component.ts
+++ Angular-RxJS-Reactive-Development/src/app/products/product-list-alt/product-detail.component.ts
@@ -1,7 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Supplier } from '../../suppliers/supplier';
 import { ProductService } from '../product.service';
-import { EMPTY, catchError, map } from 'rxjs';
+import { EMPTY, catchError, combineLatest, filter, map } from 'rxjs';
 
 @Component({
   selector: 'pm-product-detail',
@@ -29,6 +29,25 @@ export class ProductDetailComponent {
       console.error(error);
       return EMPTY;
     })
+  );
+
+  // combine all obversables into a single stream
+  // combineLatest will wait for all observables to emit at least once before emitting and return an array of the latest values
+  vm$ = combineLatest([
+    this.product$,
+    this.productSuppliers$,
+    this.pageTitle$,
+  ]).pipe(
+    // destructure the first array element [product] to get the product
+    // use boolean to check if product is undefined or null
+    filter(([product]) => Boolean(product)),
+    // destructure the entire combineLatest array and define variables for each observable
+    // map the array to an object with the variables as properties
+    map(([product, productSuppliers, pageTitle]) => ({
+      product,
+      productSuppliers,
+      pageTitle,
+    }))
   );
 
   constructor(private productService: ProductService) {}
