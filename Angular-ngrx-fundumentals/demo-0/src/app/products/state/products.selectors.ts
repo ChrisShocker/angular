d8c7766f5ddd11c7a34db7037534886ce888c9d3
Chris Andersen <Chris_Andersen@outlook.com>
2024-09-27 16:01:53 -0700

    add comments

--- Angular-ngrx-fundumentals/demo-0/src/app/products/state/products.selectors.ts
+++ Angular-ngrx-fundumentals/demo-0/src/app/products/state/products.selectors.ts
@@ -2,6 +2,12 @@ import { createFeatureSelector, createSelector } from '@ngrx/store';
 import { ProductsState } from './products.reducer';
 import { sumProducts } from 'src/app/utils/sum-products';
 
+/**
+ * Selectors are pure functions that return slices of the store state.
+ * An entire feature state can be selected using the createFeatureSelector function.
+ * The createSelector function is used to select specific slices of the state.
+ */
+
 // create a feature selector to select the products state
 // takes in the string 'products' which is the name of feature state in the store
 export const selectProductsState =
