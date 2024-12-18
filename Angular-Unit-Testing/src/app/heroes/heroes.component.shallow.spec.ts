5854658ed2d4ef7987201abf99a50498c4d18d37
Chris Andersen <Chris_Andersen@outlook.com>
2024-12-18 13:18:52 -0800

    add mock child component for HeroesComponent shallow tests

--- Angular-Unit-Testing/src/app/heroes/heroes.component.shallow.spec.ts
+++ Angular-Unit-Testing/src/app/heroes/heroes.component.shallow.spec.ts
@@ -2,7 +2,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HeroesComponent } from './heroes.component';
 import { HeroService } from '../hero.service';
 import { of } from 'rxjs';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { Component, Input, NO_ERRORS_SCHEMA, Output } from '@angular/core';
+import { Hero } from '../hero';
 
 describe('HeroesComponent (shallow tests)', () => {
   let fixture: ComponentFixture<HeroesComponent>;
@@ -11,11 +12,21 @@ describe('HeroesComponent (shallow tests)', () => {
     'addHero',
     'deleteHero',
   ]);
+
+  // mock child component hero class
+  @Component({
+    selector: 'app-hero',
+    template: '<div></div>',
+  })
+  class MockHeroComponent {
+    @Input() hero: Hero;
+    //@Output() delete = new EventEmitter();
+  }
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [HeroesComponent],
+      declarations: [HeroesComponent, MockHeroComponent],
       providers: [{ provide: HeroService, useValue: mockHeroService }],
-      schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents();
     fixture = TestBed.createComponent(HeroesComponent);
   });
