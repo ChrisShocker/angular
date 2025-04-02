4e164e19e77f014162554bf1f71f68bc94715308
Chris Andersen <chris_andersen@outlook.com>
2025-04-02 14:10:27 -0700

    Enhance app configuration and product service with HTTP client integration

--- Angular-RxJS-Signals-Fundamentals/apm-begin/src/app/app.config.ts
+++ Angular-RxJS-Signals-Fundamentals/apm-begin/src/app/app.config.ts
@@ -4,10 +4,13 @@ import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { AppData } from './app-data';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 
 // intercepts http requests with Angular API and returns
 export const appConfig: ApplicationConfig = {
   providers: [
+    // must be before InMemory.. for it to work
+    provideHttpClient(),
     importProvidersFrom(
       FormsModule,
       // api intercepts HTTP requests. AppData is defined in app-data.ts with all the applications backend data
