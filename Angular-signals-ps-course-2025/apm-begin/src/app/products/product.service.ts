06a1a30017a64356a450200645577858df122974
Chris Andersen <Christian.Andersen@odhsoha.oregon.gov>
2026-01-16 15:42:58 -0800

    FInish course with examples functionality

--- Angular-signals-ps-course-2025/apm-begin/src/app/products/product.service.ts
+++ Angular-signals-ps-course-2025/apm-begin/src/app/products/product.service.ts
@@ -1,8 +1,23 @@
-import { Injectable } from '@angular/core';
+import { httpResource } from '@angular/common/http';
+import { Injectable, signal } from '@angular/core';
+import { Product } from './product';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
   private productsUrl = 'api/products';
+
+  productsResource = httpResource<Product[]>(() => this.productsUrl, {
+    defaultValue: [],
+  });
+
+  selectedProduct = signal<Product | undefined>(undefined);
+
+  // this resource is created and called now when the function is called
+  createProducts() {
+    return httpResource<Product[]>(() => this.productsUrl, {
+      defaultValue: [],
+    });
+  }
 }
--- Angular-signals-ps-course-2025/apm-begin/src/app/reviews/review.service.ts
+++ Angular-signals-ps-course-2025/apm-begin/src/app/reviews/review.service.ts
@@ -1,9 +1,60 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { ProductService } from '../products/product.service';
+import { httpResource } from '@angular/common/http';
+import { Review } from './review';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ReviewService {
-  private reviewsUrl = 'api/reviews';
+  private readonly productService = inject(ProductService);
 
+  private readonly reviewsUrl = 'api/reviews';
+
+  // if the id is undefined don't retrieve anything
+  reviewsResource = httpResource<Review[]>(
+    () => {
+      const p = this.productService.selectedProduct();
+      if (p) {
+        return `${this.reviewsUrl}?productId=^${p.id}$`;
+      } else {
+        return undefined;
+      }
+    },
+    { defaultValue: [] },
+  );
+
+  // To use the options object
+  // reviewsResource = httpResource<Review[]>(() => ({
+  //   url: this.reviewsUrl,
+  //   method: 'GET',
+  //   headers: {
+  //     accept: 'application/json'
+  //   },
+  //   params: {
+  //     productId: `^${this.productService.selectedProduct()?.id ?? 0}$`
+  //   }
+  // }));
+
+  // Use the options object with no retrieval
+  // Prevent retrieval if there is no selected product
+  // reviewsResource = httpResource<Review[]>(() => {
+  //   const p = this.productService.selectedProduct();
+  //   if (p) {
+  //     return {
+  //       url: this.reviewsUrl,
+  //       method: 'GET',
+  //       headers: {
+  //         accept: 'application/json'
+  //       },
+  //       params: {
+  //         productId: `^${p.id}$`
+  //       }
+  //     }
+  //   } else {
+  //     return undefined;
+  //   }
+  // },
+  //   { defaultValue: [] }
+  // );
 }
